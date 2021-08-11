2455319f3f6e3bf5d74a413f06daba14dd69f78c
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-12 00:22:34 +0800

    002-Runloop与线程的关系

---
+++ proj/002-Runloop与线程的关系/02-Runloop与线程的关系/ViewController.m
@@ -0,0 +1,95 @@
+//
+//  ViewController.m
+//  02-Runloop与线程的关系
+//
+//  Created by cooci on 2018/12/5.
+//  Copyright © 2018 cooci. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LGThread.h"
+
+@interface ViewController ()
+@property (nonatomic, assign) BOOL isStopping;
+
+@end
+
+@implementation ViewController
+
+
+
+
+- (void) testOne{
+    
+    self.isStopping = NO;
+    LGThread *thread = [[LGThread alloc] initWithBlock:^{
+       
+        // thread.name = nil 因为这个变量只是捕捉
+        // LGThread *thread = nil
+        // thread = 初始化 捕捉一个nil进来
+        NSLog(@"%@---%@",[NSThread currentThread],[[NSThread currentThread] name]);
+        [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+            NSLog(@"hello word");            // 退出线程--结果runloop也停止了
+            if (self.isStopping) {
+                [NSThread exit];
+            }
+        }];
+        [[NSRunLoop currentRunLoop] run];
+    }];
+    
+    thread.name = @"lgcode.com";
+    [thread start];
+    
+}
+
+
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // 主运行循环
+     CFRunLoopRef mainRunloop = CFRunLoopGetMain();
+    
+    
+    
+    NSLog(@"111  \n %@", mainRunloop);
+    
+    
+    //
+    
+    
+    //
+    
+    
+    //
+    
+    
+    // 当前运行循环
+     CFRunLoopRef currentRunloop = CFRunLoopGetCurrent();
+    
+}
+
+
+
+
+
+
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+//    [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//
+//        NSLog(@"99999");
+//    }];
+    self.isStopping = YES;
+}
+
+
+
+
+
+@end
