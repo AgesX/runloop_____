3da52058bc0793c1941c868893b296d0c896b923
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-12 13:06:20 +0800

    LGThread *thread

--- proj/002-Runloop与线程的关系/02-Runloop与线程的关系/ViewController.m
+++ proj/002-Runloop与线程的关系/02-Runloop与线程的关系/ViewController.m
@@ -27,14 +27,14 @@
         // thread.name = nil 因为这个变量只是捕捉
         // LGThread *thread = nil
         // thread = 初始化 捕捉一个nil进来
-        NSLog(@"%@---%@",[NSThread currentThread],[[NSThread currentThread] name]);
+        NSLog(@"哈哈哈 :   %@---%@",[NSThread currentThread],[[NSThread currentThread] name]);
         [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
-            NSLog(@"hello word");            // 退出线程--结果runloop也停止了
+            NSLog(@"hello word, 走你， 子线程 timer");            // 退出线程--结果runloop也停止了
             if (self.isStopping) {
                 [NSThread exit];
             }
         }];
-        [[NSRunLoop currentRunLoop] run];
+      //  [[NSRunLoop currentRunLoop] run]; //  关键代码
     }];
     
     thread.name = @"lgcode.com";
@@ -50,12 +50,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    
+    /*
+     
+     1
     // 主运行循环
      CFRunLoopRef mainRunloop = CFRunLoopGetMain();
     
     
     
     NSLog(@"111  \n %@", mainRunloop);
+    
+    */
+    //
+    
+    
+    //
+    
+    
+    
+    
+    //
+    
+    
+    
+    //
+    
+    [self testOne];
     
     
     //
@@ -68,7 +89,7 @@
     
     
     // 当前运行循环
-     CFRunLoopRef currentRunloop = CFRunLoopGetCurrent();
+   //  CFRunLoopRef currentRunloop = CFRunLoopGetCurrent();
     
 }
 
