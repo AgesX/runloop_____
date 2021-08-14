1d40b14a6962a289a8f6067c5cda8a5d788c09b8
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-14 13:07:28 +0800

    extension MinStack

---
+++ proj/leetcode/leetcode/MinStack.swift
@@ -0,0 +1,99 @@
+
+import Foundation
+
+
+
+
+
+class MinStack {
+
+    
+    var container = [Int]()
+    
+    var alongContainer = [Int]()
+    
+    /** initialize your data structure here. */
+    init() {
+        
+    }
+    
+    func push(_ val: Int) {
+        container.append(val)
+        if let last = alongContainer.last{
+            if last >= val{
+                alongContainer.append(val)
+            }
+        }
+        else{
+            alongContainer.append(val)
+        }
+    }
+    
+    func pop() {
+        guard container.isEmpty == false else {
+            return
+        }
+        let val = container.removeLast()
+        if let last = alongContainer.last, last == val{
+            alongContainer.removeLast()
+        }
+    }
+    
+    func top() -> Int {
+        if let top = container.last{
+            return top
+        }
+        else{
+            return -1
+        }
+    }
+    
+    func getMin() -> Int {
+        if let top = alongContainer.last{
+            return top
+        }
+        else{
+            return 666
+        }
+    }
+    
+}
+
+
+
+
+extension MinStack {
+    
+    
+    func test(){
+        
+        let minS = MinStack()
+
+        minS.push(-2)  // min -2
+        minS.push(0)    // min -2
+        minS.push(-3)   // min -3
+
+
+        var tmp = minS.getMin()
+
+        print(1, tmp)
+
+        minS.pop()
+
+        tmp = minS.top()
+
+
+        print(2, tmp)
+
+
+        tmp = minS.getMin()
+
+
+        print(3, tmp)
+
+    }
+}
+
+
+
+
