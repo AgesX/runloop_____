a6f00273a0978f4494530a67c92d6088eea3ff43
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-16 15:43:17 +0800

    class Solution_island_opt{

--- proj/swift/leetcode/leetcode/TreeToMap/islandPerimeter.swift
+++ proj/swift/leetcode/leetcode/TreeToMap/islandPerimeter.swift
@@ -222,11 +222,38 @@ extension Solution_island{
 
 
 
+struct Pt{
+    let i: Int
+    let j: Int
+}
+
+
+extension Pt{
+    var key: String{
+        "\(i)_\(j)"
+    }
+    
+    
+    var lhs: Pt{
+        Pt(i: i, j: j - 1)
+    }
+    
+    var rhs: Pt{
+        Pt(i: i, j: j + 1)
+    }
+    
+    var up: Pt{
+        Pt(i: i - 1, j: j)
+    }
+    
+    var down: Pt{
+        Pt(i: i + 1, j: j)
+    }
+}
 
 
 
-
-class Solution_island___x {
+class Solution_island_opt{
     
     var visited = Set<String>()
     
@@ -266,15 +293,14 @@ class Solution_island___x {
                 
         while queue.isEmpty == false {
             let pop = queue.removeFirst()
-            let ii = pop.0
-            let jj = pop.1
-            
-            if ii >= 1{
-                let up = grid[ii - 1][jj]
-                let key = "\(ii - 1)_\(jj)"
-                if visited.contains(key) == false, up == 1{
-                    visited.insert(key)
-                    result += getEgde(grid, ii: ii - 1, jj: jj, orient: .down)
+            let pt = Pt(i: pop.0, j: pop.1)
+            if pt.i >= 1{
+                let newPt = pt.up
+                let up = grid[newPt.i][newPt.j]
+                let k = newPt.key
+                if visited.contains(k) == false, up == 1{
+                    visited.insert(k)
+                    result += getEgde(grid, pt: newPt, orient: .down)
                     
                     // result += 4 - 2
                     // 一个 dot, 四条边
@@ -285,35 +311,38 @@ class Solution_island___x {
                     //
                     
                     // 还有一种情况，共有边的转移
-                    queue.append((ii - 1, jj))
+                    queue.append((newPt.i, newPt.j))
                 }
             }
-            if ii < height - 1{
-                let down = grid[ii + 1][jj]
-                let key = "\(ii + 1)_\(jj)"
-                if visited.contains(key) == false, down == 1{
-                    visited.insert(key)
-                    result += getEgde(grid, ii: ii + 1, jj: jj, orient: .up)
-                    queue.append((ii + 1, jj))
+            if pt.i < height - 1{
+                let newPt = pt.down
+                let down = grid[newPt.i][newPt.j]
+                let k = newPt.key
+                if visited.contains(k) == false, down == 1{
+                    visited.insert(k)
+                    result += getEgde(grid, pt: newPt, orient: .up)
+                    queue.append((newPt.i, newPt.j))
                 }
             }
-            if jj >= 1{
-                let lhs = grid[ii][jj - 1]
-                let key = "\(ii)_\(jj - 1)"
+            if pt.j >= 1{
+                let newPt = pt.lhs
+                let lhs = grid[newPt.i][newPt.j]
+                let key = newPt.key
                 if visited.contains(key) == false, lhs == 1{
                     visited.insert(key)
-                    result += getEgde(grid, ii: ii, jj: jj - 1, orient: .rhs)
-                    queue.append((ii, jj - 1))
+                    result += getEgde(grid, pt: newPt, orient: .rhs)
+                    queue.append((newPt.i, newPt.j))
                 }
             }
             
-            if jj < width - 1{
-                let rhs = grid[ii][jj + 1]
-                let key = "\(ii)_\(jj + 1)"
+            if pt.j < width - 1{
+                let newPt = pt.rhs
+                let rhs = grid[newPt.i][newPt.j]
+                let key = newPt.key
                 if visited.contains(key) == false, rhs == 1{
                     visited.insert(key)
-                    result += getEgde(grid, ii: ii, jj: jj + 1, orient: .lhs)
-                    queue.append((ii, jj + 1))
+                    result += getEgde(grid, pt: newPt, orient: .lhs)
+                    queue.append((newPt.i, newPt.j))
                 }
             }
             
@@ -327,7 +356,7 @@ class Solution_island___x {
     
     
     
-    func getEgde(_ grid: [[Int]], ii: Int, jj: Int, orient direction: Direction) -> Int {
+    func getEgde(_ grid: [[Int]], pt: Pt, orient direction: Direction) -> Int {
         
         
         let height = grid.count
@@ -344,29 +373,29 @@ class Solution_island___x {
             let pop = source.removeFirst()
             switch pop {
             case .up:
-                if ii >= 1{
-                    let key = "\(ii - 1)_\(jj)"
+                if pt.i >= 1{
+                    let key = pt.up.key
                     if visited.contains(key){
                         result -= 2
                     }
                 }
             case .down:
-                if ii < height - 1{
-                    let key = "\(ii + 1)_\(jj)"
+                if pt.i < height - 1{
+                    let key = pt.down.key
                     if visited.contains(key){
                         result -= 2
                     }
                 }
             case .lhs:
-                if jj >= 1{
-                    let key = "\(ii)_\(jj - 1)"
+                if pt.j >= 1{
+                    let key = pt.lhs.key
                     if visited.contains(key){
                         result -= 2
                     }
                 }
             case .rhs:
-                if jj < width - 1{
-                    let key = "\(ii)_\(jj + 1)"
+                if pt.j < width - 1{
+                    let key = pt.rhs.key
                     if visited.contains(key){
                         result -= 2
                     }
@@ -380,3 +409,7 @@ class Solution_island___x {
     
     
 }
+
+
+
+
