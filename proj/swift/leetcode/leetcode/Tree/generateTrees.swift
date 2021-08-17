e240ccfcbc82a1e2065eeb1486a663a76af936b8
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-17 14:32:55 +0800

    func generateTrees(left

---
+++ proj/swift/leetcode/leetcode/Tree/generateTrees.swift
@@ -0,0 +1,11 @@
+//
+//  generateTrees.swift
+//  leetcode
+//
+//  Created by Jz D on 2021/8/16.
+//
+
+import Foundation
+
+
+
--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -10,6 +10,80 @@ import Foundation
 
 
 
+class Solution {
+    
+    
+    
+    func generateTrees(_ n: Int) -> [TreeNode?] {
+        var result: [TreeNode?] = []
+        guard n > 0 else {
+            return result
+        }
+        var i = 1
+        while i <= n {
+            let root = TreeNode(i)
+            
+            
+            
+            
+            
+            
+            result.append(root)
+            
+        }
+        
+        
+        
+        
+        return result
+    }
+    
+    
+    
+    func generateTrees(left lhs: Int, right rhs: Int) -> [TreeNode?]{
+        guard lhs <= rhs else {
+            if lhs == rhs{
+                return [TreeNode(lhs)]
+            }
+            else{
+                return []
+            }
+        }
+        var result: [TreeNode?] = []
+        var i = lhs
+        
+        while i <= rhs {
+            let node = TreeNode(i)
+            if i > lhs{
+                let lhsArr = generateTrees(left: lhs, right: i - 1)
+                
+            }
+            
+            if i < rhs{
+                let rhsArr = generateTrees(left: i + 1, right: rhs)
+            }
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        return result
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
