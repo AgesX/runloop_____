563438c1ad6ed62c04435eabd5c7281403c7d389
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-02 16:37:46 +0800

    // 这个是顺推     func uniquePaths

--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -9,9 +9,63 @@ import Foundation
 
 
 
-class Solution_uniquePaths_Time_Limit{
+
+
+
+
+
+class Solution_uniquePaths{
+    
+    // 这个是顺推
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        
+        guard m > 0, n > 0 else {
+            return 0
+        }
+        var container = [[Int]](repeating: [Int](repeating: 1, count: n), count: m)
+        
+        var i = 1
+        while i < m {
+            var j = 1
+            while j < n {
+                container[i][j] = container[i - 1][j] + container[i][j - 1]
+                j += 1
+            }
+            i += 1
+        }
+        return container[m - 1][n - 1]
+    }
+    
     
 
+}
+
+
+
+
+
+
+
+
+
+let result = Solution().Solution_uniquePaths(3, 2)
+
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+class Solution_uniquePaths_Time_Limit{
+    
+    // 这个是逆推
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
         let x = m - 1
         let y = n - 1
@@ -56,10 +110,24 @@ class Solution_uniquePaths_Time_Limit{
 
 
 
-let result = Solution_uniquePaths_Time_Limit().uniquePaths(3, 2)
 
 
-print(result)
+extension Solution_uniquePaths_Time_Limit{
+
+    
+    func test(){
+        
+        let result = Solution_uniquePaths_Time_Limit().uniquePaths(3, 2)
+
+
+        print(result)
+
+
+    }
+    
+    
+}
+
 
 
 
