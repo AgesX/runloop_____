3680f8032449113b68a605eea37305dff2ad5ab2
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-06 16:26:51 +0800

    //  980. Unique Paths III
    //  必须深度优先
    
    // 动态规划，就是一种深度优先

--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -6,7 +6,7 @@ import Foundation
 
 
 
-class Solution {
+class Solution_uniquePathsIII {
     
     
     struct Pos: Hashable {
@@ -14,9 +14,9 @@ class Solution {
         let y: Int
         
         func check(in grid: [[Int]], _ visited: inout Set<Pos>) -> Bool{
-            var ok = false
+            var ok = true
             if grid[x][y] == -1{
-                ok = true
+                ok = false
                 visited.insert(self)
             }
             return ok && !( visited.contains(self) )
@@ -49,12 +49,22 @@ class Solution {
         guard let start = begin, let end = final else {
             return 0
         }
+        print(start, end)
         return uniquePathsIII(grid, visited: Set([start]), m, n, start, end)
     }
     
     
     
     func uniquePathsIII(_ grid: [[Int]], visited seen: Set<Pos>, _ m: Int, _ n: Int, _ start: Pos, _ end: Pos) -> Int {
+        guard start != end else {
+           // print(seen.count)
+            if seen.count == m * n{
+                return 1
+            }
+            else{
+                return 0
+            }
+        }
         var visited = seen
         var result = 0
         if start.x > 0{
@@ -89,9 +99,32 @@ class Solution {
                 result += uniquePathsIII(grid, visited: arr, m, n, down, end)
             }
         }
-        return 0
+        return result
     }
 }
+
+
+
+
+
+extension Solution_uniquePathsIII{
+    
+    
+    func test(){
+        
+        var arr = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+
+        let result = Solution_uniquePathsIII().uniquePathsIII(arr)
+
+
+        print(result)
+        
+    }
+    
+    
+}
+
+
 
 
 
@@ -112,3 +145,21 @@ print("\n\n\n")
 
 // 就是 4 叉树，
 // 4 个分支
+
+
+
+
+
+
+
+
+//  980. Unique Paths III
+
+//  必须深度优先
+
+
+
+
+// 动态规划，就是一种深度优先
+
+
