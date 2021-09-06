7ffbde4167464df590fbea7d0e852e2394c260a1
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-06 16:13:08 +0800

    // 图，就是树
    // 图上面，一般的节点，
    
    // 就是 4 叉树，
    // 4 个分支

--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -13,8 +13,13 @@ class Solution {
         let x: Int
         let y: Int
         
-        func check(in grid: [[Int]], _ visited: Set<Pos>) -> Bool{
-            return ( grid[x][y] != -1 ) && !( visited.contains(self) )
+        func check(in grid: [[Int]], _ visited: inout Set<Pos>) -> Bool{
+            var ok = false
+            if grid[x][y] == -1{
+                ok = true
+                visited.insert(self)
+            }
+            return ok && !( visited.contains(self) )
         }
     }
     
@@ -44,44 +49,44 @@ class Solution {
         guard let start = begin, let end = final else {
             return 0
         }
-        return uniquePathsIII(grid, Set([start]), m, n, start, end)
+        return uniquePathsIII(grid, visited: Set([start]), m, n, start, end)
     }
     
     
     
-    func uniquePathsIII(_ grid: [[Int]], _ visited: Set<Pos>, _ m: Int, _ n: Int, _ start: Pos, _ end: Pos) -> Int {
-        
+    func uniquePathsIII(_ grid: [[Int]], visited seen: Set<Pos>, _ m: Int, _ n: Int, _ start: Pos, _ end: Pos) -> Int {
+        var visited = seen
         var result = 0
         if start.x > 0{
             let left = Pos(x: start.x - 1, y: start.y)
-            if left.check(in: grid, visited){
+            if left.check(in: grid, &visited){
                 var arr = visited
                 arr.insert(left)
-                result += uniquePathsIII(grid, arr, m, n, left, end)
+                result += uniquePathsIII(grid, visited: arr, m, n, left, end)
             }
         }
         if start.x <= m - 2{
             let rhs = Pos(x: start.x + 1, y: start.y)
-            if rhs.check(in: grid, visited){
+            if rhs.check(in: grid, &visited){
                 var arr = visited
                 arr.insert(rhs)
-                result += uniquePathsIII(grid, arr, m, n, rhs, end)
+                result += uniquePathsIII(grid, visited: arr, m, n, rhs, end)
             }
         }
         if start.y > 0{
-            let down = Pos(x: start.x, y: start.y - 1)
-            if down.check(in: grid, visited){
+            let up = Pos(x: start.x, y: start.y - 1)
+            if up.check(in: grid, &visited){
                 var arr = visited
-                arr.insert(down)
-                result += uniquePathsIII(grid, arr, m, n, down, end)
+                arr.insert(up)
+                result += uniquePathsIII(grid, visited: arr, m, n, up, end)
             }
         }
         if start.y <= n - 2{
             let down = Pos(x: start.x, y: start.y + 1)
-            if down.check(in: grid, visited){
+            if down.check(in: grid, &visited){
                 var arr = visited
                 arr.insert(down)
-                result += uniquePathsIII(grid, arr, m, n, down, end)
+                result += uniquePathsIII(grid, visited: arr, m, n, down, end)
             }
         }
         return 0
