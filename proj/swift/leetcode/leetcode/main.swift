f10aeacc9af3d07ef6d78c84f7448bc571584f05
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-06 16:06:42 +0800

    func uniquePathsIII(   ,    _  +

--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -12,6 +12,10 @@ class Solution {
     struct Pos: Hashable {
         let x: Int
         let y: Int
+        
+        func check(in grid: [[Int]], _ visited: Set<Pos>) -> Bool{
+            return ( grid[x][y] != -1 ) && !( visited.contains(self) )
+        }
     }
     
     
@@ -40,16 +44,46 @@ class Solution {
         guard let start = begin, let end = final else {
             return 0
         }
-        return uniquePathsIII(grid, Set([start]), m, n, end)
+        return uniquePathsIII(grid, Set([start]), m, n, start, end)
     }
     
     
     
-    func uniquePathsIII(_ grid: [[Int]], _ visited: Set<Pos>, _ m: Int, _ n: Int, _ end: Pos) -> Int {
+    func uniquePathsIII(_ grid: [[Int]], _ visited: Set<Pos>, _ m: Int, _ n: Int, _ start: Pos, _ end: Pos) -> Int {
         
-        
-        
-        
+        var result = 0
+        if start.x > 0{
+            let left = Pos(x: start.x - 1, y: start.y)
+            if left.check(in: grid, visited){
+                var arr = visited
+                arr.insert(left)
+                result += uniquePathsIII(grid, arr, m, n, left, end)
+            }
+        }
+        if start.x <= m - 2{
+            let rhs = Pos(x: start.x + 1, y: start.y)
+            if rhs.check(in: grid, visited){
+                var arr = visited
+                arr.insert(rhs)
+                result += uniquePathsIII(grid, arr, m, n, rhs, end)
+            }
+        }
+        if start.y > 0{
+            let down = Pos(x: start.x, y: start.y - 1)
+            if down.check(in: grid, visited){
+                var arr = visited
+                arr.insert(down)
+                result += uniquePathsIII(grid, arr, m, n, down, end)
+            }
+        }
+        if start.y <= n - 2{
+            let down = Pos(x: start.x, y: start.y + 1)
+            if down.check(in: grid, visited){
+                var arr = visited
+                arr.insert(down)
+                result += uniquePathsIII(grid, arr, m, n, down, end)
+            }
+        }
         return 0
     }
 }
@@ -61,3 +95,15 @@ print("\n\n\n")
 
 
 
+
+
+// 图，就是树
+
+
+
+// 图上面，一般的节点，
+
+
+
+// 就是 4 叉树，
+// 4 个分支
