a492f87a51b588de1fdacd371b0167b766b441d1
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-07 16:26:14 +0800

    Reverse Linked List

---
+++ proj/swift/leetcode/leetcode/ListNode/reverseList.swift
@@ -0,0 +1,89 @@
+//
+//  reverseList.swift
+//  leetcode
+//
+//  Created by Jz D on 2021/9/7.
+//
+
+import Foundation
+
+
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        guard head != nil else {
+            return nil
+        }
+        var current = head // 1
+        var last: ListNode? = nil
+        var final: ListNode? = nil
+        while current != nil { // 2
+            // 注意点，不要放在 cur.next,
+            // 要放在 cur
+            
+            
+            let tmp = current?.next // 2 , nil
+            current?.next = last
+            last = current  // 1 // 2
+            if tmp == nil{
+                final = current
+            }
+            current = tmp   // 2
+        }
+        return final
+    }
+    
+}
+
+
+extension Solution{
+    
+    func test(){
+        
+        let node = [1,2].arrayToLinkedList()
+
+        let result = Solution().reverseList(node)
+
+
+        print(result ?? " X ")
+
+
+
+    }
+    
+}
+
+
+
+
+extension Solution{
+    
+    func reverseList_wrong(_ head: ListNode?) -> ListNode? {
+        guard head != nil else {
+            return nil
+        }
+        var current = head // 1
+        var last: ListNode? = nil
+        var final: ListNode? = nil
+        while current?.next != nil { // 2
+            let tmp = current?.next // 2 , nil
+            current?.next = last
+            last = current  // 1 // 2
+            if tmp == nil{
+                
+                // 这里，走不到啊
+                
+                final = current
+            }
+            else{
+                current = tmp   // 2
+            }
+        }
+        return final
+    }
+}
+
+
+
+
+//  Reverse Linked List
--- proj/swift/leetcode/leetcode/main.swift
+++ proj/swift/leetcode/leetcode/main.swift
@@ -6,7 +6,6 @@ import Foundation
 
 
 
-
 print("\n\n\n")
 
 
