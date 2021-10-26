5ef4792273e01d4a66380541960e0271dc8c3cba
邓江洲 <dengjiangzhouReal@gmail.com>
2021-10-26 10:10:36 +0800

    lengthOfLastWord

--- proj/interview/interview/main.swift
+++ proj/interview/interview/main.swift
@@ -12,7 +12,39 @@ import Foundation
 
 
 
-
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        let cnt = s.count
+        guard cnt > 0 else { return 0 }
+        var current = cnt
+        var spacing = 0
+        let start = s.startIndex
+        while current >= 0{
+            if s[s.index(start, offsetBy: current)] == " "{
+                switch spacing{
+                case 0:
+                    spacing = 1
+                case 1: ()
+                case 2: spacing = 3
+                default: ()
+                }
+            }
+            else{
+                switch spacing{
+                case 0:
+                    spacing = 1
+                case 1: ()
+                case 2: spacing = 3
+                default: ()
+                }
+            }
+            current -= 1
+        }
+        
+        
+        return 0
+    }
+}
 
 
 
